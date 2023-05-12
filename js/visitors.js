6e098fc7957e75877921d405d3b81ed00d5a6571
Patrick Tang <ptang26@ptang26.local>
2023-05-11 22:01:51 -0400

    Bing_Links

--- js/visitors.js
+++ js/visitors.js
@@ -1,6 +1,6 @@
 var visitors_path = window.location.pathname
 var visitors_title = $(".post-title h3 a").text()
 
-$(".leancloud_visitors").html('<i class="fa fa-eye"></i><em class="post-meta-item-text">浏览量</em><i class="waline-pageview-count"></i></span>')
+$(".leancloud_visitors").html('<i class="fa fa-eye"></i><em class="post-meta-item-text">浏览量 </em><i class="waline-pageview-count"></i></span>')
 $(".waline-pageview-count").attr('data-path',visitors_path)
 // $(".leancloud_visitors").attr('data-flag-title',visitors_title)
